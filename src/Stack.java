39ec02513393cceee9c63abc4fa6849b5ebb9791
keira-kastelic <79609952+keira-kastelic@users.noreply.github.com>
2022-11-09 21:18:05 -0700

    Commit four
    finalizing the test driver and fixing the issues. accounting for empty stacks and more to test the methods

--- src/Stack.java
+++ src/Stack.java
@@ -1,32 +1,40 @@
 public class Stack<N> {
-    private int size = 1; // creating instance data
+    private int size = 0; // creating instance data
     private StackNode top;
     private N data;
 
-    public Stack(){
+    public Stack(){ // creating an empty stack constructor
     }
 
-    public void push(N data){
-        top = new StackNode <N> (top, data);
-        size ++;
+    public void push(N data){ // creating the push method
+        top = new StackNode <N> (top, data); // setting top to be the new node and intaking the data
+        size ++; // updating size
     }
 
-    public N pop(){
-        N data = (N) top.getData();
-        top.setData(null);
-        return data;
+    public N pop(){ // creating the pop method
+        if (isEmpty()){ // testing if the stack is empty
+            size = 0; // updating size
+            return null; // returning null
+        } else {
+            data = (N) top.getData(); // setting the data to a data variable
+            StackNode temp = top; // creating a temporary node
+            top = top.getNext(); // setting the top to the next node
+            temp.setNext(null); // setting the temp to null
+            size --; // updating size
+            return data; // returning data
+        }
     }
 
-    public N peek() {
-        return (N) top.getData();
+    public N peek() { // creating a peek method
+        return (N) top.getData(); // returning the top node data
     }
 
-    public boolean isEmpty() {
-        return top == null;
+    public boolean isEmpty() { // creating a isEmpty method
+        return top == null; // testing if the stack is empty or not
     }
 
-    public int size() {
-        return size;
+    public int size() { // creating a size method
+        return size; // retuning size
     }
 
 }
--- src/StackTestDriver.java
+++ src/StackTestDriver.java
@@ -1,21 +1,25 @@
 public class StackTestDriver {
     public static void main(String[] args) {
 
-        Stack<Integer> stack = new Stack<Integer>();
-        stack.push(1);
+        Stack<Integer> stack = new Stack<Integer>(); // creating a new stack
+        stack.push(1); // adding to the stack
         stack.push(20);
         stack.push(100);
 
-        System.out.println(stack.isEmpty());
+        System.out.println(stack.size()); // testing the size method (should print 3)
 
-        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty()); // testing if the stack is empty (should return false)
 
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        System.out.println(stack.pop());
+        System.out.println(stack.peek()); // testing peek (should print 100)
 
-        System.out.println(stack.isEmpty());
+        stack.pop(); // removes the top node
+        System.out.println(stack.pop()); // testing pop (should return 20)
+        stack.pop(); // emptying the stack
+        System.out.println(stack.pop()); // testing what happens when the stack is already emptied
+
+        System.out.println(stack.isEmpty()); // testing if the stack is empty (should return true)
+
+        System.out.println(stack.size()); // testing the size when the stack is empty (should return 0)
 
     }
 }
